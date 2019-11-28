c4f07423b1b7ef5977902e3863c9b531845c0333
smbh <smb_h@yahoo.com>
2019-11-28 09:14:09 +0330

    add sklearn for preprocess missing data

--- .ipynb_checkpoints/data_preprocessing_template-checkpoint.py
+++ .ipynb_checkpoints/data_preprocessing_template-checkpoint.py
@@ -1,12 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# for missing data 
-from sklearn.preprocessing import Imputer
 
 
-
-# import the dataset
 dataset = pd.read_csv("Data.csv")
 
 x = dataset.iloc[:, :-1].values
