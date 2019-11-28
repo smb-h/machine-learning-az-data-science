d2142615583dec5007f29f1fbd91e96114133bd2
smbh <smb_h@yahoo.com>
2019-11-28 09:56:22 +0330

    dummy variables

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 # for missing data 
 from sklearn.preprocessing import Imputer
+# encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 
@@ -17,5 +19,22 @@ y = dataset.iloc[:, 3].values
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+
+# Encoding categorical data
+label_encoder_x = LabelEncoder()
+x[:, 0] = label_encoder_x.fit_transform(x[:, 0])
+
+# dummy variables
+# convert labels to multible binary columns
+one_hot_encoder = OneHotEncoder(categorical_features=[0])
+x = one_hot_encoder.fit_transform(x).toarray()
+
+# no need for dummy variables cuz its only 2 category and it will be 0,1
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+
+
 
 
