eb105b35a2ffbc51d5b914e62d3f42a281485413
smbh <smb_h@yahoo.com>
2019-12-23 19:44:35 +0330

    minor changes

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import train_test_split
 # feature scaling
 # from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+# Backward elimination
+import statsmodels.formula.api as smf
 
 
 # import the dataset
@@ -26,12 +29,19 @@ x = onehotencoder.fit_transform(x).toarray()
 # Avoiding the dummy variable trap
 x = x[:, 1:]
 
-
 # Splitting dataset into the training set and test set
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .2, random_state = 0)
 
+# Fitting multiple linear regression to the training set
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
 
+# Predicting the test set results
+y_pred = regressor.predict(x_test)
+
+
+# Building the optimal model using backward elimination
 
 
 
