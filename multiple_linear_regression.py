7495006d01969b695c2fedd47b618594eaed9344
smbh <smb_h@yahoo.com>
2019-12-24 00:06:11 +0330

    minor changes

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,9 +6,11 @@ from sklearn.model_selection import train_test_split
 # feature scaling
 # from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 # Backward elimination
 import statsmodels.formula.api as smf
+import statsmodels.api as sm
 
 
 # import the dataset
@@ -23,8 +25,15 @@ y = dataset.iloc[:, 4].values
 # Encoding the Independent Variable
 labelencoder_X = LabelEncoder()
 x[:, 3] = labelencoder_X.fit_transform(x[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-x = onehotencoder.fit_transform(x).toarray()
+# onehotencoder = OneHotEncoder(categorical_features = [3])
+ct = ColumnTransformer(
+    # The column numbers to be transformed (here is [3])
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [3])],
+    # Leave the rest of the columns untouched
+    remainder='passthrough'
+)
+# x = onehotencoder.fit_transform(x).toarray()
+x = np.array(ct.fit_transform(x), dtype=np.float)
 
 # Avoiding the dummy variable trap
 x = x[:, 1:]
@@ -42,9 +51,15 @@ y_pred = regressor.predict(x_test)
 
 
 # Building the optimal model using backward elimination
-
-
-
+x = np.append(np.ones((50, 1)).astype(int), x, axis=1)
+x_opt = x[:, [0, 1, 2, 3, 4, 5]]
+regressor_ols = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_ols.summary()
+# removed 2 cuz it has highest p value that is greater then significant level
+x = np.append(np.ones((50, 1)).astype(int), x, axis=1)
+x_opt = x[:, [0, 1, 3, 4, 5]]
+regressor_ols = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_ols.summary()
 
 
 
